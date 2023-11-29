1ef581957c02d797ec2b9ac93a7e4b2527fa350f
SimonBoothroyd <simon.boothroyd@hotmail.com>
2023-11-29 08:31:59 -0500

    Update project and fix tests

--- plotmol/tests/test_plotmol.py
+++ plotmol/tests/test_plotmol.py
@@ -1,14 +1,13 @@
+import bokeh.plotting
 import pytest
-from bokeh.plotting import Figure
 
-from plotmol import plotmol
-from plotmol.plotmol import InputSizeError, default_tooltip_template
-from plotmol.styles import MoleculeStyle
+import plotmol
+import plotmol.styles
 
 
 @pytest.fixture()
 def bokeh_figure():
-    return Figure(tooltips=default_tooltip_template())
+    return bokeh.plotting.figure(tooltips=plotmol.default_tooltip_template())
 
 
 def test_scatter(bokeh_figure):
@@ -41,17 +40,14 @@ def test_scatter_tagged_atoms(bokeh_figure):
 
 
 def test_scatter_invalid_input_size(bokeh_figure):
-
-    with pytest.raises(InputSizeError):
+    with pytest.raises(plotmol.InputSizeError):
         plotmol.scatter(bokeh_figure, x=[0.0], y=[0.0, 1.0], smiles=["C", "CCO"])
 
 
 def test_scatter_custom_molecule_function(bokeh_figure):
-
     function_called = False
 
     def molecule_to_svg(smiles, style):
-
         nonlocal function_called
         function_called = True
 
@@ -65,7 +61,7 @@ def test_scatter_custom_molecule_function(bokeh_figure):
         x=[0.0, 1.0],
         y=[0.0, 1.0],
         smiles=["C", "CCO"],
-        molecule_style=MoleculeStyle(image_width=1),
+        molecule_style=plotmol.styles.MoleculeStyle(image_width=1),
         molecule_to_image_function=molecule_to_svg,
     )
 
@@ -73,10 +69,9 @@ def test_scatter_custom_molecule_function(bokeh_figure):
 
 
 def test_scatter_invalid_custom_data_size(bokeh_figure):
-
     expected_match = r"all have the same length: title \(2\)"
 
-    with pytest.raises(InputSizeError, match=expected_match):
+    with pytest.raises(plotmol.InputSizeError, match=expected_match):
         plotmol.scatter(
             bokeh_figure,
             x=[0.0],
@@ -87,7 +82,6 @@ def test_scatter_invalid_custom_data_size(bokeh_figure):
 
 
 def test_scatter_custom_data(bokeh_figure):
-
     glyph = plotmol.scatter(
         bokeh_figure,
         x=[0.0],
